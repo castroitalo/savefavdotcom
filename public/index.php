f563671647115dc21cf1e4c725b35a439180dca2
devcastroitalo <dev.castro.italo@gmail.com>
2023-08-24 16:57:29 -0300

    update

--- public/index.php
+++ public/index.php
@@ -1,9 +1,6 @@
 <?php
 
-use src\core\DBConnection;
 use src\core\Router;
-use src\dao\BaseDao;
-use src\dao\UserDao;
 
 /**
  * Imports app's initial settings
@@ -13,14 +10,6 @@ require __DIR__ . "/bootstrap.php";
 try {
     $router = new Router();
 
-    $userDao = new UserDao();
-    $dummyData = generate_dummy_user_data();
-
-    var_dump($userDao->createUser("cormier.stanford@hotmail.com", $dummyData["user_password"]));
-
-    /**
-     * WEB ROUTES
-     */
     $router->addRoute("GET", "/", "HomeController@homepage");
     $router->addRoute("GET", "/login", "AuthenticationController@loginPage");
     $router->addRoute("GET", "/register", "AuthenticationController@registerPage");
