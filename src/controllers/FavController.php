b70d3c614cb5783079cf890c9cd4dea853366a82
devcastroitalo <dev.castro.italo@gmail.com>
2023-08-30 19:28:37 -0300

    FavModel done and tested

---
+++ src/controllers/FavController.php
@@ -0,0 +1,21 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace src\controllers;
+
+/**
+ * Class FavController
+ *
+ * @package src\controllers;
+ */
+final class FavController 
+{
+    use BaseController;
+
+    public function addNewFav(array $params): void 
+    {
+
+    }
+}
+
